5f0e79d9e8e94527ccea081cc55717b1a9bb2d4d
dimasik <dimasik@wisebits.com>
2023-01-11 17:43:43 +0200

    0.3.0

--- src/configureContext.ts
+++ src/configureContext.ts
@@ -1,4 +1,4 @@
-import Router6, { RouteDefinition } from 'router6';
+import Router6 from 'router6';
 import { CraqAction, Registry, Store } from 'craq';
 import createHead from './createHead';
 import ClientContext from './ClientContext';
@@ -8,19 +8,19 @@ const configureContext = <T, S>(
     store,
     actions,
     components,
-    routes,
+    router,
   }: {
     actions: Registry<CraqAction<S, any>>;
     components: Registry<T>;
     store: Store<S, any>;
-    routes: RouteDefinition[];
+    router: Router6;
   },
   head = createHead(),
 ) =>
   new ClientContext(
     {
       store,
-      router: new Router6(routes),
+      router,
       registries: { actions, components },
     },
     head,
--- src/createCraqClient.ts
+++ src/createCraqClient.ts
@@ -1,73 +1,25 @@
 import { Context, actionsMiddleware } from 'craq';
-import { ForbiddenError, isRoutingError, NotFoundError } from 'router6';
 import historyMiddleware from 'router6-history';
 
-type ServerStats = {
-  actions: { [actionName: string]: boolean };
-  error: { code: number };
-};
-
-const rehydrateError = (error) => {
-  switch (error?.code) {
-    case 404:
-      return new NotFoundError(error.message, error.meta);
-    case 403:
-      return new ForbiddenError(error.message, error.meta);
-    default:
-      return undefined;
-  }
-};
-
-const createCraqClient = (
-  context: Context<any, any>,
-  App,
-  { bundles, renderers },
-) => {
-  context.router
-    .use(() => ({ to }, next) => {
-      const bundle = bundles[to.config.bundle];
-
-      return bundle().then(() => next());
-    })
-    .use(historyMiddleware())
-    .use(
-      actionsMiddleware(
-        context,
-        {
-          isServer: false,
-          executionFlow: (execution, next) => {
-            if (context.router.isStarted()) {
-              execution.catch((e) => {
-                if (isRoutingError(e)) {
-                  // @ts-ignore
-                  context.router.error = e;
-                }
-              });
-              next();
-            } else {
-              return execution.then(() => next(), next);
-            }
-          },
-        },
-        // @ts-ignore
-        (__SERVER_STATS__ as ServerStats).actions,
-      ),
-    );
+const createCraqClient = (context: Context<any, any>, { renderers }) => {
+  context.router.use(historyMiddleware()).use(
+    actionsMiddleware(context, {
+      filter: ({ options }) => options?.serverOnly !== true,
+      onSuccess: () => {},
+      onError: () => {},
+    }),
+  );
 
   let runPromise;
 
   return {
     run: (href: string) => {
       if (!runPromise) {
-        runPromise = context.router.start(
-          href,
-          // @ts-ignore
-          rehydrateError((__SERVER_STATS__ as ServerStats).error),
-        );
+        runPromise = context.router.start(href, {});
       }
 
       return {
-        render: (node: Element | Document) =>
+        render: () =>
           runPromise.then(({ config, name }) => {
             const renderer = renderers[config.renderer];
 
@@ -77,7 +29,7 @@ const createCraqClient = (
               );
             }
 
-            return renderer(context, App, { node });
+            return renderer(context);
           }),
       };
     },
